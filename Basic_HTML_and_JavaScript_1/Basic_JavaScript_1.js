48c381b685a7d6e802bf1f79c602af7ec92aaca4
Norman Teodoro <normanteodoro@hotmail.com>
2023-03-24 10:08:35 -0700

    Assignement 1

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+// alert("Hello, World!")
+console.log(document.getElementById("Header_1").innerHTML)
+
+function My_First_Function(){
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
+
+var foo = document.getElementsByTagName("p")
+console.log(foo)
